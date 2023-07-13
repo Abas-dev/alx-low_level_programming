b65e4ede3acc5725f2448f0ebe23fcb884bea364
Abas-dev <abasisaac22@gmail.com>
2023-07-13 10:32:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*the below code is going to print 
- * a string and return zero */
+/*
+ * the below code is going to print a string and return zero
+*/
 int main(void)
 {
 	char str[] = "Programming is like building a multilingual puzzle \n";
+
 	puts(str);
-	return 0;
+
+	return (0);
 }
